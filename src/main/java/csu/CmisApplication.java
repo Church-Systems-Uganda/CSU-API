349c9d7ebaaddaa16e885a170cc37e9ac44ca0d7
slubambo <slubambo57@gmail.com>
2023-01-30 15:13:19 +0300

    [feature] API initialization

---
+++ src/main/java/csu/CmisApplication.java
@@ -0,0 +1,13 @@
+package csu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmisApplication.class, args);
+	}
+
+}

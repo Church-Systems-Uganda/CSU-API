09243df7f36ba5d8655581d5611df8b9d12ef1e7
slubambo <slubambo57@gmail.com>
2023-01-30 15:38:29 +0300

    [feature] main class update

--- src/main/java/csu/CmisApplication.java
+++ src/main/java/csu/CmisApplication.java
@@ -1,10 +1,22 @@
 package csu;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = { CmisApplication.class, Jsr310JpaConverters.class })
 public class CmisApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmisApplication.class, args);

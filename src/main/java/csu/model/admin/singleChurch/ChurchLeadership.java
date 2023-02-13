9c374558df663dc5554bf3215149f2b35270c30b
marvins56 <82571414+marvins56@users.noreply.github.com>
2023-02-13 18:17:45 +0300

    church leadership, Person_church_model created

---
+++ src/main/java/csu/model/admin/singleChurch/ChurchLeadership.java
@@ -0,0 +1,108 @@
+package csu.model.admin.singleChurch;
+
+import java.util.Date;
+
+import csu.model.audit.DateAudit;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "churchLeadership")
+public class ChurchLeadership  extends DateAudit{
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "churchHierrachy_id", nullable = true)
+    private ChurchHierrachy churchHierrachy;
+
+
+    // add ministry class here
+    // @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "ministry_id", nullable = true)
+    //  private Ministry ministry;
+
+    private String Status;
+
+    @NotNull
+    private Date fromDate;
+    
+    @NotNull
+    private Date toDate;
+
+	public ChurchLeadership(ChurchHierrachy churchHierrachy, /*Ministry ministry,*/ String status, @NotNull Date fromDate,
+			@NotNull Date toDate) {
+		super();
+		this.churchHierrachy = churchHierrachy;
+		// this.ministry = ministry;
+		Status = status;
+		this.fromDate = fromDate;
+		this.toDate = toDate;
+	}
+
+	public ChurchLeadership() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public ChurchHierrachy getChurchHierrachy() {
+		return churchHierrachy;
+	}
+
+	public void setChurchHierrachy(ChurchHierrachy churchHierrachy) {
+		this.churchHierrachy = churchHierrachy;
+	}
+
+	// public Ministry getMinistry() {
+	// 	return ministry;
+	// }
+
+	// public void setMinistry(Ministry ministry) {
+	// 	this.ministry = ministry;
+	// }
+
+	public String getStatus() {
+		return Status;
+	}
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+	public Date getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public Date getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+	
+	
+    
+    
+    
+}
---
+++ src/main/java/csu/model/admin/singleChurch/PersonChurchMinistry.java
@@ -0,0 +1,48 @@
+package csu.model.admin.singleChurch;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "personChurchMinistry")
+public class PersonChurchMinistry {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String Status;
+
+     //add ministry class here
+    // @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "ministry_id", nullable = true)
+    // private Ministry ministry;
+     
+    @ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "personChurch_id", nullable = true)
+    private PersonChurch personChurch;
+    // @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "department_id", nullable = true)
+    // private Department department;
+
+
+	public PersonChurchMinistry() {
+		super();
+	}
+
+	public PersonChurchMinistry(String status, /*  Ministry ministry,*/ PersonChurch personChurch/* ,Department department*/ ) {
+		super();
+		Status = status;
+		// this.ministry = ministry;
+		this.personChurch = personChurch;
+		// this.department = department;
+	}
+    
+    
+    
+}

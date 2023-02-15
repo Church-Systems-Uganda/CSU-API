b72fa783a6008c1c13aece81e1253fd7e5f224be
marvins56 <82571414+marvins56@users.noreply.github.com>
2023-02-15 21:46:23 +0300

    [Update] Ministry model

--- src/main/java/csu/model/membership/Ministry.java
+++ src/main/java/csu/model/membership/Ministry.java
@@ -1,13 +1,18 @@
-package csu.model.Ministry;
+package csu.model.membership;
 
-import jakarta.persistence.Column;
+import csu.model.admin.Church;
+import csu.model.audit.DateAudit;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 
 @Entity

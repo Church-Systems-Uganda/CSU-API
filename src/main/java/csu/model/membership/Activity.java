a8dd21a2dbe6f48406692af41ac6ee0afa6ec880
marvins56 <82571414+marvins56@users.noreply.github.com>
2023-02-17 00:04:15 +0300

    [Update] Activity model

--- src/main/java/csu/model/membership/Activity.java
+++ src/main/java/csu/model/membership/Activity.java
@@ -1,3 +1,17 @@
+package csu.model.membership;
+
+import jakarta.persistence.Entity;
+import csu.model.admin.Church;
+import csu.model.audit.DateAudit;
+
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "activity", uniqueConstraints = { @UniqueConstraint(columnNames = { "name" }) })

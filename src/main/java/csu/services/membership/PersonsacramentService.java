f1d5df619565502c1bf98c5e19b173c8da3e1c35
slubambo <slubambo57@gmail.com>
2023-02-24 12:05:28 +0300

    :bug: fixed something before the break

--- src/main/java/csu/services/membership/PersonsacramentService.java
+++ src/main/java/csu/services/membership/PersonsacramentService.java
@@ -5,23 +5,25 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import csu.model.membership.Personsacrament;
+import csu.payload.general.ApiResponse;
 import csu.payload.membership.personsacrament.PersonsacramentPayload;
 import csu.payload.membership.personsacrament.PersonsacramentRequest;
 import csu.repository.membership.PersonsacramentRepository;
 
 public class PersonsacramentService {
-	
-	@Autowired 
+
+	@Autowired
 	PersonsacramentRepository personsacramentRepository;
-	
+
 //	get information part from a payload for the get option
-	public List<PersonsacramentPayload>  getAllPersonsacraments(){
+	public List<PersonsacramentPayload> getAllPersonsacraments() {
 		List<PersonsacramentPayload> personsacraments = new ArrayList<>();
-		for ( Personsacrament personsacrament :personsacramentRepository.findAll()) {
-			
+		for (Personsacrament personsacrament : personsacramentRepository.findAll()) {
+
 			PersonsacramentPayload personsacramentpayload = new PersonsacramentPayload();
 			personsacramentpayload.setChurch(personsacrament.getChurch());
 			personsacramentpayload.setDate(personsacrament.getDate());
@@ -30,14 +32,14 @@ public class PersonsacramentService {
 			personsacramentpayload.setPerson(personsacrament.getPerson());
 			personsacramentpayload.setSacrement(personsacrament.getSacrement());
 			personsacramentpayload.setTier(personsacrament.getTier());
-			
-		personsacraments.add(personsacramentpayload);
-		
+
+			personsacraments.add(personsacramentpayload);
+
 		}
 		return personsacraments;
 	}
 
-	//create information part for the payload
+	// create information part for the payload
 	public ResponseEntity<?> createPersonsacrament(PersonsacramentRequest request) {
 
 		if (request.getName() != null) {
@@ -50,11 +52,11 @@ public class PersonsacramentService {
 					? personsacramentRepository.findById(request.getId())
 					: Optional.empty();
 
-					Personsacrament personsacrament = existingPersonsacrament.isPresent() ? existingAffliation.get()
-					: new Personsacrament(request.getName(), request.getDate(), request.getTier(), request.getPerson(), request.getSacrement(), request.getChurch());
+			Personsacrament personsacrament = existingPersonsacrament.isPresent() ? existingPersonsacrament.get()
+					: new Personsacrament(request.getName(), request.getDate(), request.getTier(), request.getPerson(),
+							request.getSacrement(), request.getChurch());
 
-
-					Personsacrament result = personsacramentRepository.save(personsacrament);
+			Personsacrament result = personsacramentRepository.save(personsacrament);
 
 			if (result != null) {
 				return new ResponseEntity<>(new ApiResponse(true, "Personsacrament Created"), HttpStatus.OK);

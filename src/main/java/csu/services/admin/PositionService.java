06a5259533af335c2c79639fc9a2e4ec36e68573
marvins56 <82571414+marvins56@users.noreply.github.com>
2023-02-25 12:43:29 +0300

    :+1: [update] PositionService logic fixed for creation method

--- src/main/java/csu/services/admin/PositionService.java
+++ src/main/java/csu/services/admin/PositionService.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import csu.model.admin.Affliation;
 import csu.model.admin.Position;
 import csu.payload.admin.affiliation.AffiliationRequest;
 import csu.payload.admin.position.PositionPayload;
@@ -41,31 +42,39 @@ public class PositionService {
 	}
 
 	public ResponseEntity<?> createPosition(PositionRequest request) {
+
 		if (request.getName() != null) {
 
-			if (positionRepository.existsByName(request.getName())) {
-				// if true
+			Optional<Position> existingPosition = request.getId() != null
+					? positionRepository.findById(request.getId())
+					: Optional.empty();
+
+			if (!existingPosition.isPresent() && positionRepository.existsByName(request.getName())) {
 				return new ResponseEntity<>(new ApiResponse(false, "Position Exists"), HttpStatus.BAD_REQUEST);
 			}
-			// in case of edditing
-			Optional<Position> existingPosition = request.getId() != null ? positionRepository.findById(request.getId())
-					: Optional.empty();
-			// get that position if present
+
 			Position position = existingPosition.isPresent() ? existingPosition.get()
-					: new Position(request.getName(), request.getShortName());
+					: new Position();
+			
+			position.setName(request.getName());
 			position.setShortName(request.getShortName() != null ? request.getShortName() : null);
 
-			// save
 			Position result = positionRepository.save(position);
 
 			if (result != null) {
-				return new ResponseEntity<>(new ApiResponse(true, "Position Created"), HttpStatus.OK);
+				return new ResponseEntity<>(new ApiResponse(true, "position Created"), HttpStatus.OK);
 			}
 
 		}
-		return new ResponseEntity<>(new ApiResponse(false, "Error while processing request"), HttpStatus.BAD_REQUEST);
+
+		return new ResponseEntity<>(new ApiResponse(false, "position Not Created"), HttpStatus.BAD_REQUEST);
+
 	}
 
+	
+	
+	
+	
 	// delete Position
 
 	public ResponseEntity<?> deletePosition(PositionRequest request) {

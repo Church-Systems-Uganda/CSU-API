237d5e2d667c176885c654210bf7668080c53a6b
marvins56 <82571414+marvins56@users.noreply.github.com>
2023-02-27 20:39:52 +0300

    :+1: [bug Fix] Project repository

--- src/main/java/csu/repository/financeProject/ProjectRepository.java
+++ src/main/java/csu/repository/financeProject/ProjectRepository.java
@@ -1,9 +1,11 @@
 package csu.repository.financeProject;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import csu.model.finaceProjects.Project;
 
-public class ProjectRepository  extends JpaRepository<Project,Long>{
+public interface ProjectRepository extends JpaRepository<Project,Long>{
 
     
 }

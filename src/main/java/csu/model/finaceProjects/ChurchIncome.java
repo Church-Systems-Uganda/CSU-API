3fdf715c65c51a7897d7b8fea8470839be6dab04
viannejohn <vianneloro@gmail.com>
2023-02-28 16:04:36 +0300

    Entity Relationship

--- src/main/java/csu/model/finaceProjects/ChurchIncome.java
+++ src/main/java/csu/model/finaceProjects/ChurchIncome.java
@@ -3,11 +3,28 @@ package csu.model.finaceProjects;
 import java.time.LocalDate;
 
 import csu.model.admin.AffiliationChurch;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+
+@Entity
+@Table(name = "churchIncome")
 public class ChurchIncome {
+	
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String incometype;
 	private String activityname;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "church_id", nullable = true)
 	private AffiliationChurch church;
 	private LocalDate date;
 	//private Activity activity;

5e1a885d55dd2de93c74bf390ab369d53f58715b
Charles. G. Jovans <chuckjovans@gmail.com>
2023-03-17 18:51:11 +0300

    :100: >Sacrament Controller :bug: fix :accept:  :

--- src/main/java/csu/controller/membership/SacramentController.java
+++ src/main/java/csu/controller/membership/SacramentController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping; // Import Spring framework annotation for specifying request mapping
 
-import csu.payload.membership.ministy.MinistryRequest;
 import csu.payload.membership.sacrament.SacramentPayload;
 import csu.payload.membership.sacrament.SacramentRequest;
 import csu.services.membership.SacramentService;

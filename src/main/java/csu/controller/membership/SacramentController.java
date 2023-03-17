62f42736ef16f286c6fb66900093e0e43cbc7e16
Charles. G. Jovans <chuckjovans@gmail.com>
2023-03-17 18:41:08 +0300

    :apple: Sacrament Controller

--- src/main/java/csu/controller/membership/SacramentController.java
+++ src/main/java/csu/controller/membership/SacramentController.java
@@ -1,38 +1,28 @@
+// Import required libraries
 package csu.controller.membership;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired; // Import Spring framework dependency injection library
+import org.springframework.stereotype.Controller; // Import Spring framework annotation for controller
+import org.springframework.web.bind.annotation.GetMapping; // Import Spring framework annotation for handling GET requests
+import org.springframework.web.bind.annotation.RequestMapping; // Import Spring framework annotation for specifying request mapping
 
-import csu.payload.membership.ministy.MinistryPayload;
-import csu.payload.membership.sacrament.SacramentPayload;
-import csu.services.membership.SacramentService;
-
-@Controller
-@RequestMapping("/api/Membership")
+import csu.payload.membership.sacrament.SacramentPayload; 
+import csu.services.membership.SacramentService; 
+@Controller 
+@RequestMapping("/api/Membership") // Map requests for this controller to "/api/Membership"
 
 public class SacramentController {
-
-//create service
-
-	@Autowired
+	@Autowired // Use Spring dependency injection to inject the SacramentService bean
 	private SacramentService sacramentService;
 
-	// get all the Sacaraments
+	@GetMapping("/getSacraments") // Map GET requests for "/api/Membership/getSacraments" to this method
 
-	@GetMapping("/getSacraments")
-
-//	payload
-
-	public List<SacramentPayload> getAllSacraments() {
-		return sacramentService.getAllSacraments();
+	public List<SacramentPayload> getAllSacraments() { // Define method for getting all sacraments
+		return sacramentService.getAllSacraments(); // Call service method to get all sacraments and return the list of payloads
 	}
-	
-	
-	
+
 	
 	
 	

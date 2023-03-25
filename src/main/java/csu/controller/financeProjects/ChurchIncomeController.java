c47b922c97535d46aec1b6a74f2659567bca4596
viannejohn <vianneloro@gmail.com>
2023-03-25 18:12:10 +0300

    :new: Added ChurchIncomeController

--- src/main/java/csu/controller/financeProjects/ChurchIncomeController.java
+++ src/main/java/csu/controller/financeProjects/ChurchIncomeController.java
@@ -1,5 +1,39 @@
 package csu.controller.financeProjects;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import csu.payload.financeProjects.churchIncome.ChurchIncomePayload;
+import csu.services.financeProjects.ChurchIncomeService;
+
+
+@Controller
+@RequestMapping("/api/financeProjects")
+
 public class ChurchIncomeController {
+    
+@Autowired
+ChurchIncomeService churchincomeservice;
+
+//logic
+
+//get all projects
+
+@GetMapping("/getchurchincome")
+
+	public List<ChurchIncomePayload> getAllchurchIncomes(){
+		return churchincomeservice.getAllchurchIncomes();
+	}
+
+
+//create and edit projects
+
+//delete project
+
     
 }

aae38aacaf89a68c798598a8c48f097316609219
viannejohn <vianneloro@gmail.com>
2023-03-25 17:38:55 +0300

    :100: ChurchIncomeRequest created

---
+++ src/main/java/csu/payload/financeProjects/churchIncome/ChurchIncomeRequest.java
@@ -0,0 +1,48 @@
+package csu.payload.financeProjects.churchIncome;
+
+
+
+import csu.model.admin.AffiliationChurch;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class ChurchIncomeRequest {
+	private Long id;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "church_id", nullable = true)
+	private AffiliationChurch church;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public AffiliationChurch getChurch() {
+		return church;
+	}
+
+
+	public void setChurch(AffiliationChurch church) {
+		this.church = church;
+	}
+
+
+	public ChurchIncomeRequest(AffiliationChurch church) {
+		super();
+		this.church = church;
+	}
+
+	
+	
+	
+
+}

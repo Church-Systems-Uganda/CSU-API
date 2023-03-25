fbe9888f556a41deccc828701d2c0592791de02a
InTheYearOf39 <garynkuraija@gmail.com>
2023-03-25 16:42:46 +0300

    PositionController Documentation

--- src/main/java/csu/controller/admin/PositionController.java
+++ src/main/java/csu/controller/admin/PositionController.java
@@ -19,28 +19,35 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/admin")
-
 public class PositionController {
 	
 	@Autowired
 	PositionService  positionService;
 
+	/**
+	 * Get a list of all positions
+	 * @return a list of PositionPayload objects representing the positions
+	 */
 	@GetMapping("/positions")
 	public List<PositionPayload> getAllPositions() {
 		return positionService.getAllPositions();
 	}
 
+	// This endpoint is currently not in use, so it's been commented out
 //	@PostMapping("/create-position")
 //	public ResponseEntity<?> createPosition(@Valid @RequestBody PositionRequest request){
 //		
 //		return positionService.createPosition(request);
 //	}
-//	
+	
+	/**
+	 * Delete a position
+	 * @param request a PositionRequest object containing the id of the position to be deleted
+	 * @return a ResponseEntity with the appropriate status code
+	 */
 	@PostMapping("/delete-position")
 	public ResponseEntity<?> deletePosition(@RequestBody PositionRequest request) {
-
 		return positionService.deletePosition(request);
-
 	}
 
 }

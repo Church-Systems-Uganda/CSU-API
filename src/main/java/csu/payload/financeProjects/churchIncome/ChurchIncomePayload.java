be9362b6dc901c2cfccbbd140f2191b7a81917e6
viannejohn <vianneloro@gmail.com>
2023-03-25 17:22:52 +0300

    :+1: Getters and Setters

--- src/main/java/csu/payload/financeProjects/churchIncome/ChurchIncomePayload.java
+++ src/main/java/csu/payload/financeProjects/churchIncome/ChurchIncomePayload.java
@@ -1,5 +1,40 @@
 package csu.payload.financeProjects.churchIncome;
 
-public class ChurchIncomePayload {
+import csu.model.admin.AffiliationChurch;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+public class ChurchIncomePayload {
+	
+	private Long id;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "church_id", nullable = true)
+	private AffiliationChurch church;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public AffiliationChurch getChurch() {
+		return church;
+	}
+
+
+	public void setChurch(AffiliationChurch church) {
+		this.church = church;
+	}
+
+	
+	
+	
 }

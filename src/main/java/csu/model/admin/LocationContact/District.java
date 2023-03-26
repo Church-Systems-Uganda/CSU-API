79dfcef8f84740517cfc04d47af7a39008137d92
InTheYearOf39 <garynkuraija@gmail.com>
2023-03-26 14:07:18 +0300

    District Documentation

--- src/main/java/csu/model/admin/LocationContact/District.java
+++ src/main/java/csu/model/admin/LocationContact/District.java
@@ -1,6 +1,5 @@
 package csu.model.admin.LocationContact;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,33 +11,33 @@ import jakarta.persistence.UniqueConstraint;
 @Table(name = "district", uniqueConstraints = { @UniqueConstraint(columnNames = { "name" }) })
 public class District {
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String name;
+
     public Long getId() {
-		return id;
-	}
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public District(String name) {
-		super();
-		this.name = name;
-	}
+    public District(String name) {
+        super();
+        this.name = name;
+    }
 
-	public District() {
-		super();
-	}
-    
+    public District() {
+        super();
+    }
 }

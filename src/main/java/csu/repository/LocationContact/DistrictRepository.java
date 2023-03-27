74b65b1c37e3fc3cfc75808544502f2c7867e1ef
Katumbire Bob Bells <bobbells333@gmail.com>
2023-03-27 20:47:52 +0300

    District Controller

--- src/main/java/csu/repository/LocationContact/DistrictRepository.java
+++ src/main/java/csu/repository/LocationContact/DistrictRepository.java
@@ -1,5 +1,13 @@
 package csu.repository.LocationContact;
 
-public interface DistrictRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import csu.model.admin.LocationContact.District;
+
+public interface DistrictRepository extends JpaRepository<District, Long> {
+
+
+	Boolean existsByName(String name);
+	
 
 }

27df9b9727edfcc910a626aebd6819e66f67e18c
viannejohn <vianneloro@gmail.com>
2023-03-27 21:12:11 +0300

    :bug: fixed

--- src/main/java/csu/controller/admin/locationContact/CountryController.java
+++ src/main/java/csu/controller/admin/locationContact/CountryController.java
@@ -1,7 +1,5 @@
 package csu.controller.admin.locationContact;
 
-import csu.model.admin.LocationContact.Country;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;

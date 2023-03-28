deca1ea01e1a2db7ca7de12adbd25deec3e5fe84
marvins56 <95755631+Anei90@users.noreply.github.com>
2023-03-28 20:17:39 +0300

    update parishpayload

--- src/main/java/csu/payload/admin/locationContact/ParishPayload.java
+++ src/main/java/csu/payload/admin/locationContact/ParishPayload.java
@@ -1,12 +1,30 @@
 package csu.payload.admin.locationContact;
 
+import csu.model.admin.locationContact.Parish;
 
 public class ParishPayload {
     private Long id;
 	
 	
 	private String name;
+	private Parish parish;
 	
+	
+	public ParishPayload(Long id, String name, Parish parish) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.parish = parish;
+	}
+
+	public Parish getParish() {
+		return parish;
+	}
+
+	public void setParish(Parish parish) {
+		this.parish = parish;
+	}
+
 	public ParishPayload() {
 		super();
 	}

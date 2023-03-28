32eeb8ec08e48225486de1f32eb127991c5bae5a
Katumbire Bob Bells <bobbells333@gmail.com>
2023-03-28 16:22:30 +0300

    :+1:

--- src/main/java/csu/payload/admin/PersonChurchMinistry/PersonChurchMinistrypayload.java
+++ src/main/java/csu/payload/admin/PersonChurchMinistry/PersonChurchMinistrypayload.java
@@ -7,5 +7,16 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 public class PersonChurchMinistrypayload {
-	
+	public PersonChurchMinistrypayload() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 }

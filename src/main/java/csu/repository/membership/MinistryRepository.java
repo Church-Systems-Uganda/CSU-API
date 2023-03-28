fd4ca0a8f8c3d1b8fe2eb1ed3d2bf2e41a873bea
Charles. G. Jovans <chuckjovans@gmail.com>
2023-03-28 10:53:38 +0300

    :angel:  [update] Ministry Repo

--- src/main/java/csu/repository/membership/MinistryRepository.java
+++ src/main/java/csu/repository/membership/MinistryRepository.java
@@ -1,12 +1,15 @@
 package csu.repository.membership;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import csu.model.admin.Affiliation;
 import csu.model.membership.Ministry;
 
 
 public interface MinistryRepository extends JpaRepository<Ministry,Long> {
-
+	Optional<Affiliation> findByName(String name);
 	Boolean existsByName(String name);
 	
 }

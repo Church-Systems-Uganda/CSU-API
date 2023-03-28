9cecd92e860c35c86a39041c70fc976dda019c5a
triscillanandy <triscillanandy@gmail.com>
2023-03-28 17:48:17 +0300

    updated person church

--- src/main/java/csu/controller/admin/church/PersonChurchController.java
+++ src/main/java/csu/controller/admin/church/PersonChurchController.java
@@ -1,12 +1,40 @@
-package csu.controller.admin.church;
+package csu.controllers.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import csu.payload.admin.PersonChurch.PersonChurchPayload;
+import csu.payload.admin.PersonChurch.PersonChurchRequest;
+import csu.services.admin.church.PersonChurchService;
+
 @RestController
-@RequestMapping("/api/admin/")
+@RequestMapping("/api/admin/personchurch")
 public class PersonChurchController {
 
-	
-	
+	@Autowired
+	private PersonChurchService personChurchService;
+
+	@GetMapping
+	public List<PersonChurchPayload> getAllPersonChurches() {
+		return personChurchService.getAlPersonChurches();
+	}
+
+	@PostMapping
+	public ResponseEntity<?> createPersonChurch(@RequestBody PersonChurchRequest request) {
+		return personChurchService.createPersonChurch(request);
+	}
+
+	@DeleteMapping
+	public ResponseEntity<?> deletePersonChurch(@RequestBody PersonChurchRequest request) {
+		return personChurchService.deletePersonChurch(request);
+	}
+
 }

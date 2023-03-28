29e502ce55a1f6a9da49b058c39a8d325b71660d
Katumbire Bob Bells <bobbells333@gmail.com>
2023-03-28 16:25:09 +0300

    :100:

--- src/main/java/csu/payload/admin/PersonChurchMinistry/PersonChurchMinistrypayload.java
+++ src/main/java/csu/payload/admin/PersonChurchMinistry/PersonChurchMinistrypayload.java
@@ -7,16 +7,54 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 public class PersonChurchMinistrypayload {
-	public PersonChurchMinistrypayload() {
-		super();
-	}
+	  private Long id;
 
-	public Long getId() {
-		return id;
-	}
+	    private String Status;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+	   
+	    @ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "ministry_id", nullable = true)
+	    private Ministry ministry;
+	     
+	    @ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "personChurch_id", nullable = true)
+	    private PersonChurch personChurch;
+
+		public PersonChurchMinistrypayload() {
+			super();
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getStatus() {
+			return Status;
+		}
+
+		public void setStatus(String status) {
+			Status = status;
+		}
+
+		public Ministry getMinistry() {
+			return ministry;
+		}
+
+		public void setMinistry(Ministry ministry) {
+			this.ministry = ministry;
+		}
+
+		public PersonChurch getPersonChurch() {
+			return personChurch;
+		}
+
+		public void setPersonChurch(PersonChurch personChurch) {
+			this.personChurch = personChurch;
+		}
+	    
 
 }

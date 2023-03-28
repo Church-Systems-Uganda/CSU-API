11fd39f101d4576036bec302b74422374de696da
InTheYearOf39 <garynkuraija@gmail.com>
2023-03-28 17:49:30 +0300

    Location Request

--- src/main/java/csu/payload/admin/locationContact/LocationRequest.java
+++ src/main/java/csu/payload/admin/locationContact/LocationRequest.java
@@ -1,5 +1,21 @@
 package src.main.java.csu.payload.admin.locationContact;
 
+import csu.model.general.Person;
+import src.main.java.csu.model.admin.locationContact.Country;
+import src.main.java.csu.model.admin.locationContact.District;
+import src.main.java.csu.model.admin.locationContact.Region;
+
 public class LocationRequest {
+	private Long id;		
+
+	private Person person;
+
+	private District district;
+
+	private Country country;
+
+	private Region region;
+	
+	
 
 }

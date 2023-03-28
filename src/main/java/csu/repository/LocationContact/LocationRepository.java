369efab0798ef3e933499ebefd74d4251e96f6f9
InTheYearOf39 <garynkuraija@gmail.com>
2023-03-28 17:28:26 +0300

    Location Repository

---
+++ src/main/java/csu/repository/LocationContact/LocationRepository.java
@@ -0,0 +1,7 @@
+package src.main.java.csu.repository.LocationContact;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface LocationRepository extends JpaRepository<Location, Long> {
+	
+
+}

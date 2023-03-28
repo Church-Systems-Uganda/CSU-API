14f45b4e7dd8e7851f7282b270ce60ebf7a3d349
Charles. G. Jovans <chuckjovans@gmail.com>
2023-03-28 10:39:49 +0300

    :bug: fix

--- src/main/java/csu/controller/admin/locationContact/CountryController.java
+++ src/main/java/csu/controller/admin/locationContact/CountryController.java
@@ -1,4 +1,4 @@
-package csu.controller.admin.locationContact;
+package csu.controller.admin.LocationContact;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;

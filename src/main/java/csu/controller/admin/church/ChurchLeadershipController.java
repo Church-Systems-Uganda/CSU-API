c5827c85160cc3cecc57dc700bf65b1bba976010
Katumbire Bob Bells <bobbells333@gmail.com>
2023-03-28 17:01:06 +0300

    removed  unused imports

--- src/main/java/csu/controller/admin/church/ChurchLeadershipController.java
+++ src/main/java/csu/controller/admin/church/ChurchLeadershipController.java
@@ -1,10 +1,10 @@
 package csu.controller.admin.church;
 
 
-import csu.payload.general.ApiResponse;
+import csu.payload.admin.ChurchLeadership.ChurchLeadershipPayload;
+import csu.payload.admin.ChurchLeadership.ChurchLeadershipRequest;
 import csu.services.admin.church.ChurchLeadershipService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 

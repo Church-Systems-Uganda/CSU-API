8098eeeb6599bf3c064ee0d33db1014fadb1f90e
Katumbire Bob Bells <bobbells333@gmail.com>
2023-03-28 18:16:13 +0300

    solved  error

--- src/main/java/csu/controller/admin/locationContact/ParishController.java
+++ src/main/java/csu/controller/admin/locationContact/ParishController.java
@@ -1,62 +1,27 @@
 package csu.controller.admin.locationContact;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import csu.model.admin.locationContact.Parish;
 import csu.payload.admin.locationContact.ParishPayload;
 import csu.services.admin.locationContact.ParishService;
 
 @RestController
-@RequestMapping("/api/admin/")
+@RequestMapping("/api/v1/parishes")
 public class ParishController {
-	@Autowired 
-	private ParishService parishesService;
-	//get all Parishes
-	
-	   @GetMapping("/Parishes")
-	    public List<ParishPayload> getAllParishes() {
-	        // Call the getAllAffiliations method in the AffiliationService
-	        List<ParishPayload> parishes = parishesService.getAllParishes();
-	        // Return the list of affiliations
-	        return parishes;
-	    }
-	   
-	 //create a region
-	   @PostMapping("/")
-	    public ResponseEntity<parish> createParish(@RequestBody Parish parish) {
-	        Parish savedparish = parishRepository.save(parish);
-	        return new ResponseEntity<>(savedParish, HttpStatus.CREATED);
-	    }
-	    
-	//delete a region
-	    @DeleteMapping("/{id}")
-	    public ResponseEntity<HttpStatus> deleteParish(@PathVariable Long id) {
-	        Parish parish = parishRepository.findById(id).orElse(null);
-	        if (parish == null) {
-	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	        }
-	        parishRepository.delete(parish);
-	        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-	    }
-	// get region by id
-	    @GetMapping("/{id}")
-	    public ResponseEntity<parish> getParishById(@PathVariable Long id) {
-	    	Parish parish = parishRepository.findById(id).orElse(null);
-	        if (parish == null) {
-	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	        }
-	        return new ResponseEntity<>(parish, HttpStatus.OK);
-	    }
 
+    @Autowired
+    private ParishService parishService;
+
+    @GetMapping
+    public ResponseEntity<List<ParishPayload>> getAllParishes() {
+        List<ParishPayload> parishes = parishService.getAllParishes();
+        return new ResponseEntity<>(parishes, HttpStatus.OK);
+    }
 }

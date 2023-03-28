3ae7e870a4979c0a3f571a6e1e4627402b6965f1
InTheYearOf39 <garynkuraija@gmail.com>
2023-03-28 17:08:14 +0300

    Update Parish Services

--- src/main/java/csu/services/admin/locationContact/ParishService.java
+++ src/main/java/csu/services/admin/locationContact/ParishService.java
@@ -1,5 +1,30 @@
 package src.main.java.csu.services.admin.locationContact;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ParishService {
+	
+	//repository
+		@Autowired
+		private ParishRepository parishRepository;
+
+		//get all parishes
+		public List<ParishPayload> getAllParishes() {
+
+			List<ParishPayload> parishes = new ArrayList<>();
+
+			for (Parish parish : parishRepository.findAll()) {
+
+				ParishPayload payload = new ParishPayload();
+				payload.setId(parish.getId());
+				payload.setName(parish.getName());
+				
+				parish.add(payload);
+
+			}
+			return parish;
+		}
 
 }

81985d7b221335d0512c33a591341379f599556c
Katumbire Bob Bells <bobbells333@gmail.com>
2023-03-28 16:41:23 +0300

    update :+1:

--- src/main/java/csu/controller/admin/church/ChurchLeadershipController.java
+++ src/main/java/csu/controller/admin/church/ChurchLeadershipController.java
@@ -1,10 +1,12 @@
 package csu.controller.admin.church;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import csu.payload.admin.churchLeadership.ChurchLeadershipPayload;
+import csu.payload.admin.churchLeadership.ChurchLeadershipRequest;
+import csu.payload.general.ApiResponse;
+import csu.services.admin.church.ChurchLeadershipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/admin")
-public class ChurchLeadershipController {
-
-}
+import java.util.List;

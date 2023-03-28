c70e072ec5d880d3c98e68669828e7b59d8b7f25
Katumbire Bob Bells <bobbells333@gmail.com>
2023-03-28 20:00:40 +0300

    @PostMapping("/delete-churchincome") to  @DeleteMapping("/delete-churchincome")

--- src/main/java/csu/controller/financeProjects/ChurchIncomeController.java
+++ src/main/java/csu/controller/financeProjects/ChurchIncomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,7 +39,7 @@ ChurchIncomeService churchincomeservice;
 //create and edit churchIncomes
 
 //delete churchIncomes
-@PostMapping("/delete-churchincome")
+@DeleteMapping("/delete-churchincome")
 
 public ResponseEntity<?> deleteChurchIncome(@RequestBody ChurchIncomeRequest request) {
 

5772b1e94561bb4b30925c081d0e6258c6184f56
Charles. G. Jovans <chuckjovans@gmail.com>
2023-03-28 11:55:09 +0300

    :airplane: Personsacrament repo

--- src/main/java/csu/repository/membership/PersonsacramentRepository.java
+++ src/main/java/csu/repository/membership/PersonsacramentRepository.java
@@ -1,9 +1,13 @@
 package csu.repository.membership;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import csu.model.membership.Personsacrament;
 
 public interface PersonsacramentRepository extends JpaRepository <Personsacrament,Long> {
+	Optional<Personsacrament> findByName(String name);
     Boolean existsByName(String name);
 }

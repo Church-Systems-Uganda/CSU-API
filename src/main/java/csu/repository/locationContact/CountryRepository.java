25a846ce31b7667704ea8e6c6f6adb839dec6e5f
marvins56 <95755631+Anei90@users.noreply.github.com>
2023-03-28 20:17:56 +0300

    update countryrepository

--- src/main/java/csu/repository/locationContact/CountryRepository.java
+++ src/main/java/csu/repository/locationContact/CountryRepository.java
@@ -3,8 +3,12 @@ package csu.repository.locationContact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import csu.model.admin.locationContact.Country;
+import csu.payload.admin.locationContact.CountryRequest;
+import jakarta.validation.Valid;
 
 
 public interface CountryRepository extends JpaRepository<Country,Long> {
 	Boolean existsByName(String name);
+
+	Country saveAll(@Valid CountryRequest request);
 }

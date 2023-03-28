bc97c3963c1c232c208a5a51e5cdf7266dd6c2ce
InTheYearOf39 <garynkuraija@gmail.com>
2023-03-28 16:54:19 +0300

    Parish Request

--- src/main/java/csu/payload/admin/locationContact/ParishPayload.java
+++ src/main/java/csu/payload/admin/locationContact/ParishPayload.java
@@ -5,5 +5,26 @@ public class ParishPayload {
 	
 	
 	private String name;
+	
+	public ParishPayload() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }

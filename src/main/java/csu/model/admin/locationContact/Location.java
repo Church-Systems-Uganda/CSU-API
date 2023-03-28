1839a300ee8cdbae3229873698b166b49f8f249a
InTheYearOf39 <garynkuraija@gmail.com>
2023-03-28 18:08:48 +0300

    Update

--- src/main/java/csu/model/admin/locationContact/Location.java
+++ src/main/java/csu/model/admin/locationContact/Location.java
@@ -14,23 +14,27 @@ import jakarta.persistence.UniqueConstraint;
 @Entity
 @Table(name = "countries", uniqueConstraints = { @UniqueConstraint(columnNames = { "name" }) })
 public class Location {
+// The primary key for the location entity, generated automatically.	
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-      
+ // The person associated with this location (can be null).
     @ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "person_id", nullable = true)
     private Person person;
 
+ // The district associated with this location (can be null).
     @ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "district_id", nullable = true)
     private District district;
     
+ // The country associated with this location (can be null).
     @ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "country_id", nullable = true)
     private Country country;
 
+ // The region associated with this location (can be null).
     @ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "region_id", nullable = true)
 	private Region region;

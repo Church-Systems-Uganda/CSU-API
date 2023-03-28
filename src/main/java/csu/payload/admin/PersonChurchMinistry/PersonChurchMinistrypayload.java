66cc2c071c8ccde4115bbf43a4eeff586611ce18
Katumbire Bob Bells <bobbells333@gmail.com>
2023-03-28 16:21:44 +0300

    :alien:

---
+++ src/main/java/csu/payload/admin/PersonChurchMinistry/PersonChurchMinistrypayload.java
@@ -0,0 +1,11 @@
+package csu.payload.admin.PersonChurchMinistry;
+
+import csu.model.admin.Church.PersonChurch;
+import csu.model.membership.Ministry;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class PersonChurchMinistrypayload {
+	
+}

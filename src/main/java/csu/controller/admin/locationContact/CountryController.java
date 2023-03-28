14b576db003b7da258faee3f973e787e10b39d03
Katumbire Bob Bells <bobbells333@gmail.com>
2023-03-28 13:13:57 +0300

    [feature] country controller

--- src/main/java/csu/controller/admin/locationContact/CountryController.java
+++ src/main/java/csu/controller/admin/locationContact/CountryController.java
@@ -1,15 +1,28 @@
 package csu.controller.admin.LocationContact;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.List;
+import csu.model.admin.LocationContact.Country;
+import csu.repository.LocationContact.CountryRepository;
 
-@RestController
-@RequestMapping("/countries")
 public class CountryController {
-    
+	 @Autowired
+	    private CountryRepository countryRepository;
 
+	    // GET method to retrieve all countries
+	    @GetMapping("/")
+	    public ResponseEntity<List<Country>> getAllCountries() {
+	        List<Country> countries = countryRepository.findAll();
+	        return new ResponseEntity<>(countries, HttpStatus.OK);
+	    }
 }
